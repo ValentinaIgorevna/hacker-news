b923a492e5f920b0ef28a5f542697dbd1812df6c
KHodanovich Valentina <vhodanovich@vtb.ru>
2023-03-18 19:30:16 +0300

    Added hacker-news api

--- src/App.js
+++ src/App.js
@@ -1,58 +1,26 @@
 import {NewsItem} from "./NewsItem/NewsItem";
 import {useEffect, useState} from "react";
+import {get} from "./api/api";
 
-const initNews = [
-  {
-    title: 'Первая новость',
-    url: 'www.example.com',
-    username: 'Пользователь 1',
-    date: '01-01-2022',
-    score: 23,
-    id: '1'
-  },
-  {
-    title: 'Вторя новость',
-    url: 'www.example.com',
-    username: 'Пользователь 2',
-    date: '23-02-2022',
-    score: 45,
-    id: '2'
-  },
-  {
-    title: 'Третья новость',
-    url: 'www.example.com',
-    username: 'Пользователь 3',
-    date: '08-03-2022',
-    score: 75,
-    id: '3'
-  }
-]
-
-const newNews = {
-  title: 'Четвертая новость',
-  url: 'www.example.com',
-  username: 'Пользователь 4',
-  date: '09-05-2022',
-  score: 150,
-  id: '4'
-}
 
 function App() {
-  const checkStorage = () => JSON.parse(window.localStorage.getItem('newsKey')) || initNews
-  const [news, setNews] = useState(checkStorage)
+  const [news, setNews] = useState([])
 
   useEffect(() => {
-    window.localStorage.setItem('newsKey', JSON.stringify(news))
-  }, [news])
+    getNewsList()
+  }, [])
 
-  const newsCountHandler = () => {
-    setNews((prevState) => [...prevState, newNews])
+  async function getNewsList() {
+    const newsIds = await get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$priority"&limitToFirst=10')
+    const newsList = await Promise
+        .all(newsIds.map((id) => get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)))
+
+    setNews(newsList)
   }
 
   return (
     <>
       <div>Количество новостей: {news.length}</div>
-      <button onClick={newsCountHandler}>Добавить новость</button>
       {
         news.map(item => {
           return (
@@ -60,8 +28,8 @@ function App() {
                   key={item.id}
                   title={item.title}
                   url={item.url}
-                  username={item.username}
-                  date={item.date}
+                  username={item.by}
+                  date={item.time}
                   score={item.score}
               />
           )
